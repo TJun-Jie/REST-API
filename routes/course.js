563720534f70695c04262cb6f70aa1effd90d7d1
TJun-Jie <tjunjie2000@gmail.com>
2020-08-04 18:06:04 +0800

    post validation not working

--- routes/course.js
+++ routes/course.js
@@ -30,59 +30,33 @@ router.get('/courses/:id', asyncHandler( async (req, res ) => {
     res.json(specificCourse);
 }));
 
-// router.post('/courses', [
-//     // Course title must be present
-//     check('title')
-//         .not().isEmpty()
-//         .withMessage('Please Provide a title'),
-//     check('description')
-//         .exists({ checkNull: true, checkFalsy: true })
-//         .withMessage('Please Provide a description')
-//     ], bodyParser, asyncHandler( async (req, res ) => {
-//         const errors = validationResult(req);
-//         console.log(req.body)
-//         // If there are errrors
-//         if(!errors.isEmpty()) {
-//             const errorMessages = errors.array().map(error => error.msg);
-//             // Return the validation errors to the client.
-//             res.status(400).json({ errors: errorMessages });
-//         } 
-//         // If there are no errors
-//         else{
-//             const course = req.body;
-//             const newCourse = await Course.create(course);
-//             const newCourseId = newCourse.id;
-//             res.setHeader("Location", `/api/course/${newCourseId}`);
-//             res.status(201).end();
-//         }
-// }));
 router.post('/courses', [
-    check("name")
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Please provide a value for "name"'),
-  ], (req, res) => {
-    // Attempt to get the validation result from the Request object.
-    const errors = validationResult(req);
-  
-    // If there are validation errors...
-    if (!errors.isEmpty()) {
-      // Use the Array `map()` method to get a list of error messages.
-      const errorMessages = errors.array().map(error => error.msg);
-  
-      // Return the validation errors to the client.
-      res.status(400).json({ errors: errorMessages });
-    } else {
-      // Get the user from the request body.
-      const user = req.body;
-  
-      // Add the user to the `users` array.
-      console.log('good')
-  
-      // Set the status to 201 Created and end the response.
-      res.status(201).end();
-    }
-  });
-  
+    // Course title must be present
+    check('title')
+        .not().isEmpty()
+        .withMessage('Please Provide a title'),
+    check('description')
+        .exists({ checkNull: true, checkFalsy: true })
+        .withMessage('Please Provide a description')
+    ], bodyParser, asyncHandler( async (req, res ) => {
+        const errors = validationResult(req);
+        console.log(req.body)
+        // If there are errrors
+        if(!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => error.msg);
+            // Return the validation errors to the client.
+            res.status(400).json({ errors: errorMessages });
+        } 
+        // If there are no errors
+        else{
+            const course = req.body;
+            const newCourse = await Course.create(course);
+            const newCourseId = newCourse.id;
+            res.setHeader("Location", `/api/course/${newCourseId}`);
+            res.status(201).end();
+        }
+}));
+
 
 router.put('/courses/:id', bodyParser, asyncHandler( async (req,res ) => {
     const updatedInfo = req.body;
